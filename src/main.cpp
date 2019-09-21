d0fa7964df6744047af6ed42385f1e8ea661f46c
Toni <tonirosinol@hotmail.com>
2019-09-21 13:19:34 -0400

    Add header and main source

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+
+#include <project_name/project_name.h>
+
+int main(int argc, char *argv[]) {
+  // Initialize ROS node
+  ros::init(argc, argv, "project_name");
+
+  return EXIT_SUCCESS;
+}
