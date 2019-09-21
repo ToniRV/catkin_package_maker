58d00ffcdc5b3d6e462c543c042440da778e83be
Toni <tonirosinol@hotmail.com>
2019-09-21 17:05:36 -0400

    Ask first for a user name

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Ask for project name
+echo "Type project name:"
+read project_name
+
 # Store current directory
 cur_dir=$(pwd)
 
@@ -9,10 +13,6 @@ tmp_dir=$(mktemp -d /tmp/ros_node_template.XXXXXXXXX)
 # Clone repo
 echo "Cloning ros_node_template to tmp dir: $tmp_dir"
 git clone https://github.com/ToniRV/ros_node_template.git $tmp_dir
-
-# Ask for project name
-echo "Type project name:"
-read project_name
 
 # Make catkin package
 cd $tmp_dir
